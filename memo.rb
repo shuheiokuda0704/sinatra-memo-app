dac2de9921c0691218bfde7784524fd4a0ee329b
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2022-01-31 21:22:33 +0900

    Create routes

--- memo.rb
+++ memo.rb
@@ -1,5 +1,33 @@
 require 'sinatra'
 
 get '/' do
-  'hello'
+  'redirect to memos'
+end
+
+get '/memos' do
+  'show memos page'
+end
+
+get '/memos/:id' do
+  'show memos/:id page'
+end
+
+get '/memos/new' do
+  'show memos/new page'
+end
+
+get '/memos/:id/edit' do
+  'show memos/:id/edit page'
+end
+
+post '/memos' do
+  'create new memo'
+end
+
+patch '/memos/:id' do
+  'update memo'
+end
+
+delete '/memos/:id' do
+  'delete memo'
 end

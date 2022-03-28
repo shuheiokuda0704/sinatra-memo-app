2d3b85b2ff80af0012b26a4ccb2a7d5223e7db61
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2022-03-28 10:32:43 +0900

    Fix review comment - Change CONN to @@conn - Prevent SQL Injection

--- models/memo.rb
+++ models/memo.rb
@@ -6,7 +6,7 @@ class Memo
   attr_accessor :id, :title, :content
 
   DB_NAME = 'memos'
-  CONN = PG.connect(dbname: DB_NAME)
+  @@conn = PG.connect(dbname: DB_NAME)
 
   def initialize(id, title, content)
     @id = id
@@ -15,8 +15,8 @@ class Memo
   end
 
   def self.create(params)
-    CONN.exec_params('INSERT INTO memos (title, content) VALUES ($1::text, $2::text);', [params[:title], params[:content]])
-    res = CONN.exec('SELECT * FROM memos ORDER BY id DESC LIMIT 1').first
+    @@conn.exec_params('INSERT INTO memos (title, content) VALUES ($1::text, $2::text);', [params[:title], params[:content]])
+    res = @@conn.exec('SELECT * FROM memos ORDER BY id DESC LIMIT 1').first
     Memo.new(res['id'].to_i, res['title'], res['content'])
   end
 
@@ -24,20 +24,20 @@ class Memo
     target = Memo.find(@id.to_i)
     return nil unless target
 
-    CONN.exec('UPDATE memos SET title = $1::text, content = $2::text WHERE id = $3::int;', [params[:title], params[:content], @id.to_i])
+    @@conn.exec_params('UPDATE memos SET title = $1::text, content = $2::text WHERE id = $3::int;', [params[:title], params[:content], @id.to_i])
 
     Memo.find(@id.to_i)
   end
 
   def self.all
-    res = CONN.exec('SELECT * FROM memos ORDER BY id')
+    res = @@conn.exec('SELECT * FROM memos ORDER BY id')
     res.map do |record|
       Memo.new(record['id'].to_i, record['title'], record['content'])
     end
   end
 
   def self.find(memo_id)
-    res = CONN.exec("SELECT * FROM memos WHERE id = #{memo_id}").first
+    res = @@conn.exec_params('SELECT * FROM memos WHERE id = $1::int', [memo_id.to_i]).first
     return nil unless res
 
     Memo.new(res['id'].to_i, res['title'], res['content'])
@@ -47,7 +47,7 @@ class Memo
     target = Memo.find(@id.to_i)
     return nil unless target
 
-    CONN.exec("DELETE FROM memos WHERE id = #{@id.to_i}")
+    @@conn.exec_params('DELETE FROM memos WHERE id = $1::int', [@id.to_i])
     target
   end
 end

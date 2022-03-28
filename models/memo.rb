ab9a2daaaafc930b87cdc106f4ecc5844311f536
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2022-03-28 10:48:34 +0900

    Use instance var & class instance var

--- models/memo.rb
+++ models/memo.rb
@@ -6,17 +6,18 @@ class Memo
   attr_accessor :id, :title, :content
 
   DB_NAME = 'memos'
-  @@conn = PG.connect(dbname: DB_NAME)
+  @class_conn = PG.connect(dbname: DB_NAME)
 
   def initialize(id, title, content)
     @id = id
     @title = title
     @content = content
+    @instance_conn = PG.connect(dbname: DB_NAME)
   end
 
   def self.create(params)
-    @@conn.exec_params('INSERT INTO memos (title, content) VALUES ($1::text, $2::text);', [params[:title], params[:content]])
-    res = @@conn.exec('SELECT * FROM memos ORDER BY id DESC LIMIT 1').first
+    @class_conn.exec_params('INSERT INTO memos (title, content) VALUES ($1::text, $2::text);', [params[:title], params[:content]])
+    res = @class_conn.exec('SELECT * FROM memos ORDER BY id DESC LIMIT 1').first
     Memo.new(res['id'].to_i, res['title'], res['content'])
   end
 
@@ -24,20 +25,20 @@ class Memo
     target = Memo.find(@id.to_i)
     return nil unless target
 
-    @@conn.exec_params('UPDATE memos SET title = $1::text, content = $2::text WHERE id = $3::int;', [params[:title], params[:content], @id.to_i])
+    @instance_conn.exec_params('UPDATE memos SET title = $1::text, content = $2::text WHERE id = $3::int;', [params[:title], params[:content], @id.to_i])
 
     Memo.find(@id.to_i)
   end
 
   def self.all
-    res = @@conn.exec('SELECT * FROM memos ORDER BY id')
+    res = @class_conn.exec('SELECT * FROM memos ORDER BY id')
     res.map do |record|
       Memo.new(record['id'].to_i, record['title'], record['content'])
     end
   end
 
   def self.find(memo_id)
-    res = @@conn.exec_params('SELECT * FROM memos WHERE id = $1::int', [memo_id.to_i]).first
+    res = @class_conn.exec_params('SELECT * FROM memos WHERE id = $1::int', [memo_id.to_i]).first
     return nil unless res
 
     Memo.new(res['id'].to_i, res['title'], res['content'])
@@ -47,7 +48,7 @@ class Memo
     target = Memo.find(@id.to_i)
     return nil unless target
 
-    @@conn.exec_params('DELETE FROM memos WHERE id = $1::int', [@id.to_i])
+    @instance_conn.exec_params('DELETE FROM memos WHERE id = $1::int', [@id.to_i])
     target
   end
 end
